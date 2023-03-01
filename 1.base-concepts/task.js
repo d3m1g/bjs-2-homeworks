f1a7d0602ebf76aba53988eec3c620874dbf15e7
Alexander Sambur <demik86@mail.ru>
2023-03-02 00:55:56 +1000

    поправил первое, всё равно тест не прходит :(

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -6,16 +6,16 @@ function solveEquation(a, b, c) {
   let d = Math.pow(b, 2) - 4*a*c;
 
   if (d < 0) {
-    console.log('Корней нет')
-  } else if (d = 0) {
-    let rootOne = -b/(2*a);
-    arr [0] = rootOne;
+    console.log('Корней нет');
+  } else if (d === 0) {
+    rootOne = -b / (2*a);
+    arr.push(rootOne);
     console.log(arr);
   } else {
-    rootOne = (-b + Math.sqrt(d) )/(2*a);
-    rootTwo = (-b - Math.sqrt(d) )/(2*a);
-    arr [0] = rootOne;
-    arr [1] = rootTwo;
+    rootOne = (-b + Math.sqrt(d)) / (2 * a);
+    rootTwo = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(rootOne);
+    arr.push(rootTwo);
     console.log('первый корень' + arr[0] + ';' + 'второй корень' + arr[1]);
   }
   return arr;
@@ -30,7 +30,7 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   isNaN(percent);
   isNaN(contribution);
   isNaN(amount);
-  isNaN(countMonths);
+  isNaN(countMonths)
   ) {
     console.log('что-то пошло не так. smth went wrong');
     return false;
