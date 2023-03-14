8181baead26d49173b93aac3f4bfc373c485d5d1
Alexander Sambur <demik86@mail.ru>
2023-03-14 22:24:59 +1000

    третье задание выполнено

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,29 @@
+// function compareArrays(arr1, arr2) {
+//  if (arr1.length != arr2.length) {
+//   return false;
+//  }
+
+//  for (let i = 0; i < arr1.length; i++) {
+//   if (arr1[i] !== arr2[i]) {
+//    return false;
+//   }
+//  }
+
+//   return true; 
+// }
+
+
 function compareArrays(arr1, arr2) {
-  
+ return arr1.length === arr2.length && arr1.every((element, i) => element === arr2[i]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+ let result = users.filter(users => users.gender === gender).map(users => users.age).reduce((acc, item, index, arr) => {
+  acc += item;
+  if (index === arr.length - 1) {
+   return acc / arr.length;
+  }
+  return acc;
+ }, 0)
+ return result;
 }
