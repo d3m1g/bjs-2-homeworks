e8547e525169028e10c07d2c169c54d47f4e6c6a
Alexander Sambur <demik86@mail.ru>
2023-03-26 03:07:56 +1000

    Задание 6. Не выбрасывает ошибку

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,52 @@
-﻿
+﻿function parseCount(smthToParse) {
+ const final = Number.parseFloat(smthToParse);
+ if (final === NaN) {
+  throw new Error("Невалидное значение");
+ }
+ return final;
+}
+
+function validateCount(smthToParse) {
+ try {
+  return parseCount(smthToParse);
+ } catch (error) {
+  return error;
+ }
+}
+
+class Triangle {
+ constructor(a, b, c) {
+  this.a = a;
+  this.b = b;
+  this.c = c;
+  if ((a + b) < c || (a + c) < b || (b + c) < a) {
+   throw new Error("Треугольник с такими сторонами не существует");
+  }
+ }
+
+ get perimeter() {
+  return this.a + this.b + this.c;
+ }
+
+ get area() {
+  const halfPerimeter = this.perimeter / 2;
+  return parseFloat(Math.sqrt(
+   (halfPerimeter * (halfPerimeter - this.a) * (halfPerimeter - this.b) * (halfPerimeter - this.c))
+   ).toFixed(3));
+ }
+}
+
+function getTriangle(a, b, c) {
+ try {
+  return new Triangle(a, b, c);
+ } catch (error) {
+  return {
+   get perimeter() {
+    return "Ошибка! Треугольник не существует";
+   },
+   get area() {
+    return "Ошибка! Треугольник не существует";
+   }
+  }
+ }
+}
