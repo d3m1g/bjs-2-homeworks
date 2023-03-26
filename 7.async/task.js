4fb02b52edd93ca79e8411d1e5bf80c4fbaf6aff
Alexander Sambur <demik86@mail.ru>
2023-03-26 22:52:30 +1000

    7 задание. не работает removeClock

---
+++ 7.async/task.js
@@ -0,0 +1,53 @@
+class AlarmClock {
+ constructor(alarmCollection, intervalId) {
+  this.alarmCollection = [];
+  this.intervalId = null;
+ }
+
+ addClock(time, callback) {
+  if (arguments.length < 2) {
+   throw new Error("Отсутствуют обязательные аргументы");
+  }
+
+  if (this.alarmCollection.includes(time)) {
+   console.warn('Уже присутствует звонок на это же время');
+  }
+
+  this.alarmCollection.push({ callback, time, canCall: true });
+ }
+
+ removeClock(time) {
+  this.alarmCollection = this.alarmCollection.filter(element => element.time === time);
+ }
+
+ getCurrentFormattedTime() {
+  return new Date().toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+ }
+
+ start() {
+  if (this.intervalId !== null) {
+   return;
+  }
+  this.intervalId = setInterval(() =>
+   this.alarmCollection.forEach(alarm => {
+    if (alarm.time === this.getCurrentFormattedTime() && alarm.canCall) {
+     alarm.canCall = false;
+     alarm.callback();
+    }
+   }), 1000);
+ }
+
+ stop() {
+  clearInterval(this.intervalId);
+  this.intervalId = null;
+ }
+
+ resetAllCalls() {
+  this.alarmCollection.forEach(alarm => alarm.canCall = true);
+ }
+
+ clearAlarms() {
+  this.stop();
+  this.alarmCollection = [];
+ }
+}
